5ed7de7dc4d9ff03186824e06db9b8de80fab482
SumanJK <girisuman289@gmail.com>
2022-12-19 17:24:10 +0530

    updated server

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,6 +4,6 @@ require("dotenv").config();
 //!  connecting to dataBase
 
 module.exports = () =>{
-    mongoose.connect(process.env.DATABASE);
+    mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true});
     
 };
